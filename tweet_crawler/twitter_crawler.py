4ddcceb98a053f34256521ada5d72bd31c62d0a3
EC2 Default User <ec2-user@ip-172-31-20-159.ec2.internal>
2022-11-26 20:12:57 +0000

    found downloaded tweets

--- tweet_crawler/twitter_crawler.py
+++ tweet_crawler/twitter_crawler.py
@@ -1,6 +1,8 @@
 import glob
+import pickle
 import pandas as pd
-from typing import Callable, Generator, List, Literal, TypeVar
+from tqdm import tqdm as progress
+from typing import Callable, Generator, List, Literal, Set, TypeVar
 
 
 CrawlerType = Literal["test", "full"]
@@ -17,25 +19,33 @@ class TweetCrawler:
 
     def __init__(self, crawler_type: CrawlerType):
         self.dfs = self.read_dfs(crawler_type)
-        self.parquet_count = -1 if crawler_type == "test" else 0  # todo: find these
+        self.parquet_count = 1 if crawler_type == "test" else 11
 
     def read_dfs(self, crawler_type: CrawlerType) -> List[pd.DataFrame]:
         if crawler_type == 'full':
             paths = [file for file in glob.glob(f'{DATA_PATH}/*parquet*') if 'likes' not in file]
         else:
             paths = [f'{DATA_PATH}/senators_115.parquet.gzip']
-        return [pd.read_parquet(path) for path in paths]
+        return [pd.read_parquet(path) for path in progress(paths, desc='Reading dataframes')]
 
     def apply_function(self, callable: Callable[[pd.DataFrame], T]) -> Generator[T, None, None]:
         for df in self.dfs:
             yield callable(df)
 
 
+def get_ids(df: pd.DataFrame) -> Set[int]:
+    return set(df['id'].to_list())
+
+
 def main():
-    test_crawler = TweetCrawler('full')
-    print_generator = test_crawler.apply_function(print)
-    for d in print_generator:
-        d
+    full_crawler = TweetCrawler('full')
+    total_set = set()
+    id_sets = full_crawler.apply_function(get_ids)
+    for _set in id_sets:
+        total_set.update(_set)
+
+    with open('downloaded_tweet_ids.pickle', 'wb') as fp:
+        pickle.dump(total_set, fp)
 
 
 if __name__ == "__main__":
