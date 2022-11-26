5bf9b79578ba2e36359855693996f704bd4e2624
EC2 Default User <ec2-user@ip-172-31-20-159.ec2.internal>
2022-11-26 20:00:10 +0000

    dev edits to crawler

--- tweet_crawler/twitter_crawler.py
+++ tweet_crawler/twitter_crawler.py
@@ -1,10 +1,13 @@
-from typing import Callable, Generator, List, Literal, TypeVar
-import pandas as pd
 import glob
+import pandas as pd
+from typing import Callable, Generator, List, Literal, TypeVar
 
 
 CrawlerType = Literal["test", "full"]
 T = TypeVar("T")
+
+
+DATA_PATH = '/home/ec2-user/congressional_tweets'
 
 
 class TweetCrawler:
@@ -17,16 +20,22 @@ class TweetCrawler:
         self.parquet_count = -1 if crawler_type == "test" else 0  # todo: find these
 
     def read_dfs(self, crawler_type: CrawlerType) -> List[pd.DataFrame]:
-        paths = [file for file in glob.glob("**/*.parquet*") if "likes" not in file]
+        if crawler_type == 'full':
+            paths = [file for file in glob.glob(f'{DATA_PATH}/*parquet*') if 'likes' not in file]
+        else:
+            paths = [f'{DATA_PATH}/senators_115.parquet.gzip']
         return [pd.read_parquet(path) for path in paths]
 
     def apply_function(self, callable: Callable[[pd.DataFrame], T]) -> Generator[T, None, None]:
         for df in self.dfs:
             yield callable(df)
 
-            
+
 def main():
-    pass
+    test_crawler = TweetCrawler('full')
+    print_generator = test_crawler.apply_function(print)
+    for d in print_generator:
+        d
 
 
 if __name__ == "__main__":
