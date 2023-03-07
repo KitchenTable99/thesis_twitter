c0610c0f191602fdf99de91a0985a62adb7a137c
KitchenTable99 <ccbitt23@colby.edu>
2023-03-07 16:52:24 -0500

    depth2 extraction is a go

--- tweet_crawler/twitter_crawler.py
+++ tweet_crawler/twitter_crawler.py
@@ -87,6 +87,38 @@ def main():
             fp.write('\n')
 
 
+def get_user_tweets(df):
+    return df.groupby(['author_id'])
+
+
+def extract_depth_2():
+    likes_crawler = TweetCrawler('depth_2')
+    grouped_dfs = likes_crawler.apply_function(get_user_tweets)
+
+    with open('fuckups.pickle', 'rb') as fp:
+        fuckups = pickle.load(fp)
+    fuckups_dict = {f: [] for f in fuckups}
+
+    user_depth_features = []
+
+    for grouped_df in grouped_dfs:
+        for group in grouped_df.groups:
+            if group in fuckups:
+                fuckups_dict[group].append(grouped_df.get_group(group))
+            else:
+                depth_features = extract_depth_features(grouped_df.get_group(group))
+                user_depth_features.append(depth_features)
+
+    for fuckup_list in fuckups_dict.values():
+        single_df = pd.concat(fuckup_list)
+        depth_features = extract_depth_features(single_df)
+        user_depth_features.append(depth_features)
+
+    all_depth_features = pd.DataFrame(user_depth_features)
+    print(all_depth_features)
+
+    all_depth_features.to_parquet('user_depth_features.parquet.gzip', compression='gzip', index=False)
+
 def likes():
     likes_crawler = TweetCrawler('depth_2')
     author_ids = likes_crawler.apply_function(get_user_ids)
