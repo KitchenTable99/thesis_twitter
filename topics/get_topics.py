1f9db0b9d44c40c4cb92ecfff04b4a0be7758893
KitchenTable99 <ccbitt23@colby.edu>
2023-04-04 08:52:36 -0400

    set up tweet topic extraction

---
+++ topics/get_topics.py
@@ -0,0 +1,23 @@
+from top2vec import Top2Vec
+import pandas
+import sqlite3
+
+
+def assign_fake_topics():
+    pass
+
+
+def main():
+    pass
+    # count tweets
+    # create a generator to yeild ids
+    # create callable tokenizer
+    # run through tweets grabbing text and id
+    # get subset of tweet ids
+    # pass tweet ids and callable to model
+    # extract topic num and confidence
+    # write to sqlite
+
+
+if __name__ == "__main__":
+    main()
